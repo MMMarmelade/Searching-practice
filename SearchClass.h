143e984112c1f4bb45abbf15af1d5d6f4aa156f1
MMMarmelade <46017980+MMMarmelade@users.noreply.github.com>
2019-07-09 19:32:38 +0800

    Create SearchClass.h

---
+++ SearchClass.h
@@ -0,0 +1,235 @@
+#pragma once
+#include "searchingH.h"
+#include <iostream>
+class SearchClass
+{
+public:
+	SearchClass();
+	~SearchClass();
+	/*函数定义在LinearSearch.cpp中*/
+	int Search(char k, int last, struct records L[]);
+	/*********************
+	*在F所指的链表中搜索包含关键字k的元素记录，并返回其指针
+	**********************/
+	LinkedList LinkedSearch(char k, LinkedList F) {
+		while (F != NULL) {
+			if (F->data.keyc == k) {
+				return F;
+			}
+			else
+				F = F->next;
+		}
+		return F;
+	}
+	/******************
+	*折半排序
+	*搜索有序排序的顺序表
+	************************/
+	int BinarySearch(char k, int last, LinearList F) {
+		int low, up, mid;
+		low = 0;
+		up = last;
+		while (low <= up) {
+			mid = (low + up) / 2;
+			if (F[mid].keyc > k) {
+				up = mid - 1;
+			}
+			else if (F[mid].keyc < k) {
+				low = mid + 1;
+			}
+			else
+				return F[mid].num;
+		}
+		return 0;
+		
+	}
+	/********************
+	*线性分块搜索
+	*blockL为每块长度，index数组元素为每组最大
+	*********************/
+	int LinearBlockedSearch(char k, int last, int blockL, char index[], LinearList F) {
+		int i, j;//i是索引序号，j是组内序号
+		i = 0;
+		j = 1;
+		while (index[i] < k) {
+			i++;
+			if (i == last) {
+				return 0;
+			}
+		}
+		while (j < blockL+1) {
+			if (F[i * blockL + j].keyc == k) {
+				return F[i * blockL + j].num;
+			}
+			else
+				j++;
+		}
+		return 0;
+	}
+
+	/********************
+	*在带索引的分块链表中搜索元素k
+	*blocks是分块数
+	*找到则返回其指针
+	*/
+	LinkedList linkedBlockedSearch(char k,int blocks,LinkedList LL[]) {
+		int i=0;//索引序号
+		while (LL[i]->data.keyc < k) {
+			i++;
+			if (i == blocks)
+				return NULL;
+		}
+		LinkedList F = LL[i];
+		while (F) {
+			if (F->data.keyc == k) {
+				return F;
+			}
+			else
+				F = F->next;
+		}
+		return NULL;
+	}
+	/********************************
+	*往带索引的链表中插入k、n
+	*********************************/
+	bool linkedBlockedInsert(char k,int n,int blocks,LinkedList LL[]) {
+		int i = 0;
+		while (LL[i]->data.keyc < k) {
+			i++;
+			if (i == blocks)
+				return 0;
+		}
+		LinkedList F = LL[i];
+		while (F) {
+			if (F->data.keyc == k) {
+				return 0;//不插入相同元素
+			}
+			else
+				F = F->next;
+		}
+		LinkedList tempL = new celltype;
+		tempL->data.keyc = k;
+		tempL->data.num = n;
+		tempL->next = LL[i]->next;
+		LL[i]->next = tempL;
+		return 1;
+	}
+	/**********************************
+	*删除带索引分块链表中的关键字为k的元素
+	***********************************/
+	bool linkedBlockedDelete(char k, int blocks, LinkedList LL[]) {
+		int i = 0;//索引序号
+		while (LL[i]->data.keyc < k) {
+			i++;
+			if (i == blocks)
+				return 0;
+		}
+		LinkedList B = LL[i],F = LL[i];//B为上一个，F为当前
+		while (F) {
+			if (F->data.keyc == k) {
+				if (B == F) {
+					return 0;//不删索引
+				}
+				else {
+					B->next = F->next;
+					delete LL[i];//从内存中删除
+					return 1;
+				}
+			}
+			else {
+				B = F;
+				F = F->next;
+			}
+		}
+		return 0;
+	}
+
+	/*****************************
+	*在二叉查找树中搜索
+	******************************/
+	BST bstSearch(char k, BST F) {
+		if (F == NULL) {
+			return NULL;
+		}
+		else if (k == (*F).data.keyc) {
+			return F;
+		}
+		else if (k > F->data.keyc) {
+			bstSearch(k, F->rchild);
+		}
+		else
+			bstSearch(k, F->lchild);
+	}
+	/*****************************
+	*在二叉查找树F中插入记录为R的元素
+	*参数&F，在需要地址可变的时候，要用引用，
+	******************************/
+	void bstInsert(records R, BST& F) {
+		if (!F) {
+			F = new treenode;/**创建对象时会被赋予新地址***/
+			F->data = R;
+			F->lchild = NULL;
+			F->rchild = NULL;
+		}
+		else if (R.keyc < F->data.keyc)
+			bstInsert(R, F->lchild);
+		else if (R.keyc > F->data.keyc)
+			bstInsert(R, F->rchild);
+	}
+
+	/*****************************
+	*在二叉查找树中删除某子树中关键字最小的结点（删除结点有两个非空子树时，右子树中最左边的结点代替被删的结点）
+	*返回该结点数据，删除该结点并让其子树继承（最小结点只可能有右子树）
+	******************************/
+	records bstDeleteMin(BST &F) {
+		records tmp;
+		BST P;
+		if (!F->lchild) {
+			P = F;
+			tmp = F->data;
+			F = F->rchild;/**若参数中F不带引用&，则函数运行后该地址并不会改变**/
+			delete P;/**若参数中F不带引用&，上句地址不改变，则树中的一个结点被破坏，后续操作会报错**/
+			return tmp;
+		}
+		else
+			return bstDeleteMin(F->lchild);
+	}
+	/*************************
+	*删除树中结点
+	**************************/
+	void bstDelete(char k, BST& F) {
+		if (F) {
+			if (k < F->data.keyc)
+				bstDelete(k, F->lchild);
+			else if (k > F->data.keyc)
+				bstDelete(k, F->rchild);
+			else if (F->lchild == NULL)
+				F = F->rchild;
+			else if (F->rchild == NULL)
+				F = F->lchild;
+			else
+				F->data = bstDeleteMin(F->rchild);
+		}
+	}
+	/*************************
+	*先根遍历
+	***************************/
+	void PreOrder(BST F) {
+		if (F) {
+			std::cout << F->data.keyc << std::endl;
+			PreOrder(F->lchild);
+			PreOrder(F->rchild);
+		}
+	}
+	/*************************
+	*中根遍历
+	*中序遍历二叉查找树能按关键字大小顺序遍历
+	***************************/
+	void InOrder(BST F) {
+		if (F) {
+			InOrder(F->lchild);
+			std::cout << F->data.keyc << std::endl;
+			InOrder(F->rchild);
+		}
+	}
+};
