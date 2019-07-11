e8e154d2d393890ea8ee7f7cdfa7451f8df9f4cf
MMMarmelade <46017980+MMMarmelade@users.noreply.github.com>
2019-07-11 17:44:24 +0800

    Update searchingH.h

--- searchingH.h
+++ searchingH.h
@@ -1,6 +1,8 @@
 #pragma once
 /*测试记录元素顺序列表格式*/
 struct records {
+	records() {};
+	records(char c, int n) :num(n), keyc(c) {};
 	int num;
 	char keyc;
 };
@@ -12,10 +14,20 @@ struct celltype {
 	celltype* next;
 };
 typedef celltype* LinkedList;//LinkedList必须是指针
-
 /*二叉查找树元素*/
 struct treenode {
 	records data;
 	treenode* lchild, * rchild;
 };
 typedef treenode* BST;
+/*AVL树结点*/
+struct avlnode {
+	/*初始化h为1、bf为0，很重要，在后续程序中认为空指针高度为0*/
+	avlnode(records d) :
+		data(d), h(1), bf(0), rchild(nullptr), lchild(nullptr) {};
+	records data;
+	int h;/*height*/
+	int bf;/*balance factor*/
+	avlnode* rchild, * lchild;
+};
+typedef avlnode* avlTree;
